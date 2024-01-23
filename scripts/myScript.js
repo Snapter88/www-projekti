9cff474700451969462577a91879ffc163417141
Snapter88 <142223357+Snapter88@users.noreply.github.com>
2024-01-24 00:07:52 +0200

    Update
    Google fontin testaus ja pieniä parannuksia mobiili näkymään

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -2,12 +2,9 @@
 
   $(document).ready(function(){
     $(".btn-news").click(function(){
-      $(".form-container").show(100);
+      $(".news-container").show(100);
     });
     $(".cancelbtn").click(function(){
-      $(".form-container").hide();
-    });
-    $(".news-container").click(function(){
-      $(".form-container").hide();
+      $(".news-container").hide();
     });
   });
